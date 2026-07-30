6d3bbdfd67f3b0d2cf2f1ae8df1cb40df7093b9d
pierrenormand908-hub <pierrenormand908@gmail.com>
2026-07-30 18:39:15 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
+// Écoute l'ordre d'afficher une notification (même app fermée)
+self.addEventListener('message', (event) => {
+    if (event.data && event.data.type === 'PROGRAMMER_NOTIF') {
+        const { id, texte, delai } = event.data;
 
+        setTimeout(() => {
+            self.registration.showNotification("Rappel de tâche", {
+                body: texte,
+                icon: "icon.png", // Optionnel : met le nom de ton icône si tu en as une
+                badge: "icon.png",
+                tag: id.toString()
+            });
+        }, delai);
+    }
+});
